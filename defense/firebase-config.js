15b495a6bf36bd07eab6c5ae65e67406b0f890df
Mar10 <iba.dc00117@gmail.com>
2026-08-24 18:33:23 +0300

    Delete defense directory

--- defense/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC2jNNzAkghmVE6KLOeGPtd3CCTzpw3qo",
-  authDomain: "leaderboard-90b9b.firebaseapp.com",
-  projectId: "leaderboard-90b9b",
-  storageBucket: "leaderboard-90b9b.firebasestorage.app",
-  messagingSenderId: "891059392275",
-  appId: "1:891059392275:web:757305992c2d83d39214e6",
-  measurementId: "G-RXNVYRFXC5"
-};
